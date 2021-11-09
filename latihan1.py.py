6153711556c74b0befe336e196eeef97729a5def
unknown <saehanardelino@gmail.com>
2021-11-09 10:48:07 +0700

    Menambahkan Program dan SS

---
+++ latihan1.py.py
@@ -0,0 +1,7 @@
+from random import random
+n = int(input("MASUKAN JUMLAH N :"))
+while n==n:
+    break
+for x in range(n):
+    bil1=random()%0.5
+    print(bil1)
---
+++ program1.py.py
@@ -0,0 +1,14 @@
+pengusaha = 100000000
+a=0*pengusaha
+b=0*pengusaha
+c=0.01*pengusaha
+d=0.01*pengusaha
+e=0.05*pengusaha
+f=0.05*pengusaha
+g=0.05*pengusaha
+h=0.02*pengusaha
+bulan = [a, b, c, d, e, f, g, h]
+for y in range(len(bulan)):
+    print("LABA BULAN KE-",y+1, "SEBESAR :",bulan[y])
+    z = a+b+c+d+e+f+g+h
+print("TOTAL LABA ADALAH :", z)
